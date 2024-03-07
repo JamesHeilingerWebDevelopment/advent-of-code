c70c8704edf00f91f58ccddbce250d19522e405d
TheRealSpaceMonkey <james.heilinger@gmail.com>
2024-03-07 10:00:54 -0800

    2022, days 1 & 2

---
+++ 2022/01/python/code.py
@@ -0,0 +1,31 @@
+from sys import argv
+
+
+def read_input():
+    with open(argv[1], "r") as fp:
+        return fp.read().split("\n\n")
+
+
+def part_1(chunks):
+    inventory = []
+    for chunk in chunks:
+        inventory.append(sum([int(x) for x in chunk.split("\n")]))
+
+    top_inventory = []
+    for i in range(3):
+        top_inventory.append(max(inventory))
+        inventory.remove(top_inventory[i])
+
+
+    print(f"Part 1: {top_inventory[0]}")
+    return top_inventory
+
+
+def part_2(top_three):
+    print(f"Part 2: {sum(top_three)}")
+
+
+if __name__ == "__main__":
+    # Part 1 answer: 
+    # Part 2 answer: 
+    part_2(part_1(read_input()))  # Correct answer: 
